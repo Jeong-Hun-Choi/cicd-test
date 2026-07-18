7038b00bcedc2863073e36061b35c27d53509062
최정훈 <cjhcross2@daum.net>
2026-07-18 11:57:43 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/example/cicdtest/CicdTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.cicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTestApplication.class, args);
+	}
+
+}
+//git config --global user.email "cjhcross2@daum.net"
