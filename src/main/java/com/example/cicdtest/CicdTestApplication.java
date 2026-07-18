71636d309e0710a4c90b42beaa013bbde172c1d0
Jeong-Hun-Choi <cjhcross2@daum.net>
2026-07-18 20:21:47 +0900

    Initial commit

---
+++ src/main/java/com/example/cicdtest/CicdTestApplication.java
@@ -0,0 +1,16 @@
+package com.example.cicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("CI/CD Test Application 시작8");
+		
+		SpringApplication.run(CicdTestApplication.class, args);
+	}
+
+}
+//git config --global user.email "cjhcross2@daum.net"
