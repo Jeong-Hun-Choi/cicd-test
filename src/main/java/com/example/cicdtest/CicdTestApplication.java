faa1e1596a3041545fce7d7f0ad696e2e9fcd49e
Jeong-Hun-Choi <cjhcross2@daum.net>
2026-07-19 03:57:55 +0900

    Update application class

--- src/main/java/com/example/cicdtest/CicdTestApplication.java
+++ src/main/java/com/example/cicdtest/CicdTestApplication.java
@@ -7,10 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CicdTestApplication {
 
 	public static void main(String[] args) {
-		System.out.println("CI/CD Test Application 시작8");
+		System.out.println("CI/CD Test Application 시작9");
 		
 		SpringApplication.run(CicdTestApplication.class, args);
 	}
 
 }
 //git config --global user.email "cjhcross2@daum.net"
+//git clone https://github.com/Jeong-Hun-Choi/cicd-test.git
