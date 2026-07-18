3da6d06f0260dcc8366d11043572764aa15ee1fe
최정훈 <cjhcross2@daum.net>
2026-07-18 12:23:28 +0900

    Add startup message

--- src/main/java/com/example/cicdtest/CicdTestApplication.java
+++ src/main/java/com/example/cicdtest/CicdTestApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CicdTestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("CI/CD Test Application 시작");
+		
 		SpringApplication.run(CicdTestApplication.class, args);
 	}
 
